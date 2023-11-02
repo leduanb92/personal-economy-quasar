96c6901061af7e1692a6ad663ca2fb01414c4cd9
Leduan Rosell <leduanb@gmail.com>
2023-11-02 16:50:15 -0400

    Initial commit

---
+++ src/pages/AccountsPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex column flex-center">
+    <q-icon name="wallet" size="lg" color="accent"></q-icon>
+    <h2 class="text-primary">Your accounts</h2>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'AccountsPage'
+})
+</script>
