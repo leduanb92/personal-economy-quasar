bf46ef6e1102dbb23288b5d65b35c725b6ebd509
Leduan Rosell <leduanb@gmail.com>
2024-02-16 16:52:54 -0600

    feat(axios client): Set local server address as baseUrl and basic settings

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -7,7 +7,16 @@ import axios from 'axios'
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: 'https://api.example.com' })
+const api = axios.create({
+  baseURL: 'https://localhost:8036',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  }
+})
+api.defaults.xsrfCookieName = 'csrftoken';
+api.defaults.xsrfHeaderName = 'X-CSRFToken';
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
