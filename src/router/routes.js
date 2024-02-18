6cab5e15a4a6df4f5b85424e4d0a9c32ace4340c
Leduan Rosell <leduanb@gmail.com>
2024-02-17 22:55:48 -0600

    feat(routes): New route to home page with auth flow

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,25 @@
 const routes = [
   {
+    path: "/home",
+    component: () => import("layouts/PublicLayout.vue"),
+    children: [
+      { path: "", name: "home", component: () => import("pages/HomePage.vue") },
+    ],
+  },
+  {
     path: "/",
     component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: "", component: () => import("pages/IndexPage.vue") },
-      { path: "accounts/", component: () => import("pages/AccountsPage.vue") },
+      {
+        path: "",
+        name: "dashboard",
+        component: () => import("pages/IndexPage.vue"),
+      },
+      {
+        path: "accounts/",
+        name: "account-list",
+        component: () => import("pages/AccountsPage.vue"),
+      },
     ],
   },
 
