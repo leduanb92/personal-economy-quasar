edd81fb3d769a96b2249a5f6bd61c7e950d3bf7a
Leduan Rosell <leduanb@gmail.com>
2024-03-14 23:43:50 -0600

    Changed default notification timeout

--- src/boot/notify-defaults.js
+++ src/boot/notify-defaults.js
@@ -2,7 +2,7 @@ import { Notify } from "quasar";
 
 Notify.setDefaults({
   position: "bottom",
-  timeout: 3500,
+  timeout: 3000,
   textColor: "white",
   progress: true,
   actions: [{ icon: "r_close", color: "white" }],
