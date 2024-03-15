7492334a59274d8e4d387d761d1bb32c50987970
Leduan Rosell <leduanb@gmail.com>
2024-03-15 00:13:49 -0600

    feat(accounts): Improve UX in Accounts page.
    Show notification messages when actions are completed (save, edit, delete) and when there were errors.
    Show confirmation dialog before deleting an account.
    Show loading animation when requesting accounts

--- src/pages/AccountsPage.vue
+++ src/pages/AccountsPage.vue
@@ -1,9 +1,9 @@
 <template>
   <q-page class="flex column flex-center">
     <transition enter-active-class="animated fadeIn" mode="out-in">
-      <div v-if="!accounts.length" class="flex column flex-center">
+      <div v-if="!accounts.length && !loading" class="flex column flex-center">
         <q-icon name="r_wallet" size="76px" color="primary"></q-icon>
-        <h2 class="text-primary">Your accounts</h2>
+        <h2 class="text-primary">You do not have any account yet</h2>
       </div>
       <div v-else class="q-pa-md q-gutter-md flex flex-center">
         <transition-group
@@ -23,27 +23,32 @@
       </div>
     </transition>
     <q-page-sticky position="bottom-right" :offset="[18, 18]">
-      <q-btn fab icon="r_add" color="secondary" @click="toggleModal" />
+      <q-btn fab icon="r_add" color="primary" @click="toggleModal" />
     </q-page-sticky>
     <account-modal
       v-model="accountModal.value"
       :account="accountModal.account"
       @saved="onSavedAccount"
-      @cancelled="accountModal.account = null"
+      @cancelled="
+        () => {
+          accountModal.account = null;
+        }
+      "
     />
   </q-page>
 </template>
 
 <script setup>
-import { computed, onMounted, reactive, ref } from "vue";
+import { computed, inject, onMounted, reactive, ref, watch } from "vue";
 import { useAccountsStore } from "stores/accounts-store";
-import { useAuthStore } from "stores/auth-store";
+import { useWorkspaceStore } from "stores/workspace-store";
 import accountsServer from "src/server/accounts";
 import AccountModal from "components/accounts/AccountModal.vue";
 import AccountCard from "components/accounts/AccountCard.vue";
 
 const accountsStore = useAccountsStore();
-const authStore = useAuthStore();
+const workspaceStore = useWorkspaceStore();
+const bus = inject("bus");
 
 const accountModal = reactive({
   value: false,
@@ -52,12 +57,36 @@ const accountModal = reactive({
 const loading = ref(false);
 const showMessage = ref(false);
 const message = ref("");
-const messageTimeout = ref(4000);
-const errors = ref({});
+const messageType = ref("positive");
+const messageDelay = ref(3000);
 
 const accounts = computed(() =>
   accountsStore.accounts.toSorted((a, b) => a.name.localeCompare(b.name))
 );
+
+watch(loading, () => {
+  if (loading.value) {
+    workspaceStore.showLoading({
+      message: "Loading accounts...",
+      delay: 700,
+    });
+  } else {
+    workspaceStore.hideLoading();
+  }
+});
+watch(showMessage, () => {
+  if (showMessage.value) {
+    workspaceStore.showNotification({
+      message: message.value,
+      type: messageType.value,
+      timeout: messageDelay.value,
+      onDismiss: () => {
+        showMessage.value = false;
+        messageDelay.value = 3000;
+      },
+    });
+  }
+});
 
 //Methods
 const toggleModal = function () {
@@ -66,6 +95,7 @@ const toggleModal = function () {
 const onSavedAccount = function (account) {
   accountModal.account = null;
   message.value = "The account was saved successfully";
+  messageType.value = "positive";
   showMessage.value = true;
   refresh();
 };
@@ -76,22 +106,43 @@ const onEditAccount = function (account) {
 };
 
 const onDeleteAccount = function (account) {
+  workspaceStore.showConfirmDialog({
+    title: "Delete Account",
+    message: "Are you sure you want to delete this account?",
+    ok: {
+      label: "Delete",
+      color: "negative",
+      icon: "r_delete",
+      flat: true,
+    },
+    cancel: {
+      icon: "r_close",
+      flat: true,
+    },
+    onOk: () => {
+      deleteAccount(account);
+    },
+  });
+};
+const deleteAccount = (account) => {
   accountsServer
     .deleteAccount(account.id)
     .then(() => {
       message.value = "The account was deleted successfully";
+      messageType.value = "positive";
       showMessage.value = true;
       refresh();
     })
     .catch((error) => {
       if (error.response && error.response.status === 401) {
-        // EventBus.dispatch("logout");
+        bus.dispatch("logout");
       } else {
-        const errorData = error.response.data;
+        const errorData = error.response?.data || {};
         message.value =
-          errorData["generalErrors"] ||
-          errorData["message"] ||
+          errorData["generalErrors"]?.join(". ") ||
+          errorData["message"]?.join(". ") ||
           "There was an error deleting the account";
+        messageType.value = "negative";
         showMessage.value = true;
       }
     });
@@ -105,19 +156,17 @@ const refresh = () => {
     })
     .catch((error) => {
       if (error.response && error.response.status === 401) {
-        // EventBus.dispatch("logout");
-        authStore.logout();
-        window.location.reload();
+        bus.dispatch("logout");
       } else {
         if (error.response) {
-          errors.value = error.response.data;
+          message.value = Object.values(error.response.data).flat().join(". ");
         } else {
-          errors.value = {
-            error: [
-              "There was an error loading your accounts. Please, refresh the page.",
-            ],
-          };
+          message.value =
+            "There was an error loading your accounts. Please, refresh the page.";
         }
+        messageType.value = "negative";
+        messageDelay.value = 6000;
+        showMessage.value = true;
       }
     })
     .finally(() => {
